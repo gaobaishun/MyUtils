d4e3990d087dbd9b7cd9d30f29d2fde33727d6c5
gaobaishun <gaobaishun@foxmail.com>
2024-06-15 22:09:53 +0800

    first commit

---
+++ src/main/java/search/BinarySearch.java
@@ -0,0 +1,31 @@
+package search;
+
+/**
+ * 二分法搜索
+ * 适用于查找已排序数组
+ * 时间复杂度 log2n
+ * 空间复杂度 log2n
+ */
+public class BinarySearch {
+
+    public static boolean search(int[] array,int target,int start,int end){
+        if(end<start){
+            return false;
+        }
+
+        int helfNum=(end+start)/2;
+        if(array[helfNum]==target){
+            return true;
+        }else if(array[helfNum]>target){
+            return search(array,target,start,helfNum-1);
+        }else{
+
+            return search(array,target,helfNum+1,end);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a={1,4,7,11,15};
+        System.out.println(search(a,4,0,a.length-1));
+    }
+}
